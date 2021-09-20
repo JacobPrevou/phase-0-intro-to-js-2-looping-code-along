4bacceab01a882a24cc1aa5dfc07e3a3dfd62c98
Jake <prevou15@gmail.com>
2021-09-20 18:59:18 -0500

    commit tree 961c4cd8108fb5c2e56611d9591435e184b6d6b1

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+let names = ['Lisa', 'Kaitlin', 'Jan']
+let surprise = 'surprise';
+
+function writeCards(names, surprise) {
+    let array = [];
+    for(let i = 0; i < names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${surprise} gift!`
+        array.push(message)
+    }
+    return array
+}
+
+
+function countDown() {
+    let countdown = 10;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
+
+// let countup = 0;
+// while (countup < 10) {
+//   console.log(countup++);
+// }
