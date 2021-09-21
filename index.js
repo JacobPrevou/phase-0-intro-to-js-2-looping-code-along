d299d058ad826c1903cda3ff664f858fa579faf5
Jake <prevou15@gmail.com>
2021-09-20 19:00:16 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let names = ['Lisa', 'Kaitlin', 'Jan']
+let surprise = 'surprise';
+
+function writeCards(names, surprise) {
+    let array = [];
+    for(let i = 0; i < names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${surprise} gift!`
+        array.push(message)
+    }
+    return array
+}
+
+
+function countDown() {
+    let countdown = 10;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
